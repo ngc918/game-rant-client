5c1cfa5cf4a1d2636ade5f008a5ec7c3d4d320da
Nicolas Gonzalez <98353175+ngc918@users.noreply.github.com>
2022-07-28 23:50:36 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import Register from "./pages/Register";
+
+function App() {
+	return (
+		<div className="App">
+			<Register />
+		</div>
+	);
+}
+
+export default App;
